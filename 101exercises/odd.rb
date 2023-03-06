4bd23a3b9736efba4ba0ddc6b7671371e57cd48b
JakeB <butlerjake23@gmail.com>
2023-03-06 04:50:54 +0000

    new file odd

---
+++ 101exercises/odd.rb
@@ -0,0 +1,16 @@
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def number_abso(number)
+  if number%2 != 0
+    prompt("Your number #{number} is odd!!")
+  else
+    prompt("Your number #{number} is even!!")
+  end
+end
+
+prompt("Enter a number that is odd:")
+odd_number = gets.chomp.to_i
+
+number_abso(odd_number)
